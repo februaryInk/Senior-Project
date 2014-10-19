4241b1694abe41c82e85ecad2a87958f2eb60856
Farrah Brink <febrink@gmail.com>
2014-10-19 13:49:42 -0400

    Add site name and page title helper methods.
    The site name and page title helper methods ensure consistency and make
    the site name and page titles more easily changed. Tests were added to
    check that the titles are correctly included on their pages.
    
        - Added the name helper to application_helper.rb.
        - Added the page title helpers to core_pages_helper.rb.
        - Amended spec_helper.rb so the helper methods would be included in
          tests.
        - Wrote title tests in core_pages_controller_test.rb.
        $ git add .
        $ git commit -F commit-message.txt
        $ git push origin master

--- app/helpers/core_pages_helper.rb
+++ app/helpers/core_pages_helper.rb
@@ -1,2 +1,12 @@
 module CorePagesHelper
+
+    def about_title
+    
+        "About #{ site_name }"
+    end
+    
+    def home_title
+    
+        "#{ site_name }"
+    end
 end
--- test/controllers/core_pages_controller_test.rb
+++ test/controllers/core_pages_controller_test.rb
@@ -1,14 +1,16 @@
 require 'test_helper'
 
 class CorePagesControllerTest < ActionController::TestCase
-  test "should get about" do
+
+  test 'should get about' do
     get :about
     assert_response :success
+    assert_select 'title', about_title
   end
 
-  test "should get home" do
+  test 'should get home' do
     get :home
     assert_response :success
+    assert_select 'title', home_title
   end
-
 end
