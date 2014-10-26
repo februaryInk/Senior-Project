872e3001252bd7b4630f26886b566ed19654ea88
Farrah Brink <febrink@gmail.com>
2014-10-25 20:33:14 -0400

    Add help page title helper methods.
    The page title helper methods ensure consistency and make the site name
    and page titles more easily changed. Tests were added to check that the
    titles are correctly included on their pages.
    
        - Added the page title helpers to help_pages_helper.rb.
        - Amended spec_helper.rb so the helper methods would be included in
          tests.
        - Wrote title tests in help_pages_controller_test.rb.
        $ git add .
        $ git commit -F commit-message.txt
        $ git push origin master

--- app/helpers/help_pages_helper.rb
+++ app/helpers/help_pages_helper.rb
@@ -1,2 +1,22 @@
 module HelpPagesHelper
+
+    def contact_title
+    
+        "Contact"
+    end
+
+    def faq_title
+    
+        "#{ site_name } FAQ"
+    end
+    
+    def getting_started_title
+    
+        "Getting Started on #{ site_name }"
+    end
+
+    def help_title
+        
+        "#{ site_name } Help Center"
+    end
 end
--- test/controllers/help_pages_controller_test.rb
+++ test/controllers/help_pages_controller_test.rb
@@ -1,24 +1,28 @@
 require 'test_helper'
 
 class HelpPagesControllerTest < ActionController::TestCase
-  test "should get contact_us" do
-    get :contact_us
-    assert_response :success
-  end
 
-  test "should get faq" do
-    get :faq
-    assert_response :success
-  end
+    test 'should get contact' do
+        get :contact
+        assert_response :success
+        assert_select 'title', contact_title
+    end
 
-  test "should get getting_started" do
-    get :getting_started
-    assert_response :success
-  end
+    test 'should get faq' do
+        get :faq
+        assert_response :success
+        assert_select 'title', faq_title
+    end
 
-  test "should get help_center" do
-    get :help_center
-    assert_response :success
-  end
+    test 'should get getting_started' do
+        get :getting_started
+        assert_response :success
+        assert_select 'title', getting_started_title
+    end
 
+    test 'should get help_center' do
+        get :help_center
+        assert_response :success
+        assert_select 'title', help_title
+    end
 end
