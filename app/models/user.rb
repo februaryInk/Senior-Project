073388c0ff75e6790e7212bab184867791a46b6f
Farrah Brink <febrink@gmail.com>
2014-11-01 17:15:38 -0400

    Add used password validations.
    The function has_secure_password was added to the user model to handle
    most of the validation work, and consequently a column password_digest
    was added to the user table in the database and the password hashing is
    accomplished via BCrypt.
    
        - Added password validations to user.rb.
        - Added password_digest column to migration create_users.rb.
        $ rake db:drop RAILS_ENV=development
        $ rake db:migrate RAILS_ENV=development
        $ rake db:drop RAILS_ENV=test
        $ rake db:migrate RAILS_ENV=test
        $ git add .
        $ git commit -F commit-message.txt
        $ git push origin master

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,12 @@
 class User < ActiveRecord::Base
     EMAIL_REGEX = /\A([\w+\-.]+)(@)([a-z\d\-]+)(?:\.[a-z\d\-]+)*(\.)([a-z]+)\z/i
     USERNAME_REGEX = /\A(?=.*[a-z0-9].*)([a-z0-9\-_.]+)([\s][a-z0-9\-_.]+)*\z/i
+    
+    has_secure_password(  ) # creates password and password_confirmation attributes
 
     validates :email, { :format => { :with => EMAIL_REGEX }, :presence => true, :uniqueness => { :case_sensitive => false } }
     validates :username, { :format => { :with => USERNAME_REGEX }, :length => { :maximum => 32 }, :presence => true }
+    validates :password, { :length => { :maximum => 32, :minimum => 10 } }
     validates :simple_name, { :uniqueness => true }
     
     before_validation(  ) { self.simple_name = self.username.downcase.gsub( /[ \-\._\s ]/, "" ) }
