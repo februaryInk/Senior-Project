3728528a44117201b91cbe76ac970326224bb423
Farrah Brink <febrink@gmail.com>
2014-11-01 12:13:05 -0400

    Add user page title helper methods.
        - Added the page title helpers to users_helper.rb.
        - Added titles to pages.
        - Amended spec_helper.rb so the helper methods would be included in
          tests.
        - Wrote title tests in users_controller_test.rb.
        $ git add .
        $ git commit -F commit-message.txt
        $ git push origin master

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,2 +1,17 @@
 module UsersHelper
+
+    def users_index_title
+    
+        "#{ site_name } Members"
+    end
+
+    def users_new_title
+    
+        "#{ site_name } Signup"
+    end
+    
+    def users_show_title
+    
+        'Account Overview'
+    end
 end
--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -1,24 +1,26 @@
 require 'test_helper'
 
 class UsersControllerTest < ActionController::TestCase
-  test "should get edit" do
-    get :edit
-    assert_response :success
-  end
+    test 'should get edit' do
+        get :edit, :id => 1
+        assert_response :success
+    end
 
-  test "should get index" do
-    get :index
-    assert_response :success
-  end
+    test 'should get index' do
+        get :index
+        assert_response :success
+        assert_select 'title', users_index_title
+    end
 
-  test "should get new" do
-    get :new
-    assert_response :success
-  end
+    test 'should get new' do
+        get :new
+        assert_response :success
+        assert_select 'title', users_new_title
+    end
 
-  test "should get show" do
-    get :show
-    assert_response :success
-  end
-
+    test 'should get show' do
+        get :show, :id => 1
+        assert_response :success
+        assert_select 'title', users_show_title
+    end
 end
