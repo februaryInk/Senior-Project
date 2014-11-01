622791e40cff13e1aec024ddb95e9595eb5b13ae
Farrah Brink <febrink@gmail.com>
2014-11-01 14:33:33 -0400

    Add email and username validation tests.
    A username must:
    
        - Be present;
        - be less than 32 characters;
        - consist of at least one alphanumeric character, spaces, underscores,
          periods, and/or dashes;
        - be unique, even with regard to case and when stripped of spaces,
          underscores, periods, and/or dashes; and,
        - not have spaces at the beginning or end.
    
    An email must:
    
        - Be present;
        - be unique;
        - be case-insensitive;
        - follow the proper email format; and,
        - be saved in all lower-case.
    
        - Added appropriate tests to user_test.rb
        - Emptied users.yml.
        $ git add .
        $ git commit -F commit-message.txt
        $ git push origin master

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,7 +1,85 @@
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+    
+    def setup
+        @user = User.new( :email => 'testuser@example.com', :username => 'Test User' )
+    end
+    
+    test 'should be valid' do
+        assert @user.valid?
+    end
+    
+    test 'username should be present' do
+        @user.username = '   '
+        assert_not @user.valid?
+    end
+    
+    test 'username that must be unique' do
+        duplicate_name_user = @user.dup
+        duplicate_name_user.email = 'otheremail@example.com'
+        duplicate_name_user.username = @user.username.upcase
+        @user.save
+        assert_not duplicate_name_user.valid?
+        
+        duplicate_name_user.username = "-- #{ @user.username }-.-"
+        assert_not duplicate_name_user.valid?
+    end
+    
+    test 'username should not be more than 32 characters' do
+        @user.username = 'a' * 33
+        assert_not @user.valid?
+    end
+    
+    test 'username should register as valid if it has a valid format' do
+        valid_usernames = [ "Alpha Beta", "7Alpha.Beta", "..Gamma9", "--Gamma--", "777" ]        
+        valid_usernames.each do | username |
+            @user.username = username
+            assert @user.valid?, "#{ username.inspect } should be valid"
+        end
+    end
+    
+    test 'username should register as invalid if it has an invalid format' do
+        invalid_usernames = [ "Alpha Beta ", " Alpha Beta", "$amma", "@#!&%", "-.-" ]        
+        invalid_usernames.each do | username |
+            @user.username = username
+            assert_not @user.valid?, "#{ username.inspect } should be invalid"
+        end
+    end
+    
+    test 'email should be present' do
+        @user.email = '   '
+        assert_not @user.valid?
+    end
+    
+    test 'email should be unique, even with regard to case' do
+        duplicate_email_user = @user.dup
+        duplicate_email_user.email = @user.email.upcase
+        duplicate_email_user.username = 'Some Other username'
+        @user.save
+        assert_not duplicate_email_user.valid?
+    end
+    
+    test 'email should not be more than 255 characters' do
+        @user.username = 'a' * 256
+        assert_not @user.valid?
+    end
+    
+    test 'email should register as valid if it has a valid format' do
+        valid_addresses = %w[ user@alpha.COM A_US-ER@f.b.org frst.lst@alpha.jp a+b@gamma.cn ]
+        
+        valid_addresses.each do | address |
+            @user.email = address
+            assert @user.valid?, "#{ address.inspect } should be valid"
+        end
+    end
+    
+    test 'email should register as invalid if it has an invalid format' do
+        invalid_addresses = %w[ alpha@beta..com user@alpha,com user_at_alpha.org example.user@alpha. alpha@beta_gamma.com alpha@beta+gamma.com ]
+        
+        invalid_addresses.each do | address |
+            @user.email = address
+            assert_not @user.valid?, "#{ address.inspect } should be invalid"
+        end
+    end
 end
