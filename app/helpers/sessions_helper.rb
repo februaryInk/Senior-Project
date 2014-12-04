e1e1bb00d3379cf5824f068610ea5fa03fbfccc2
Farrah Brink <febrink@gmail.com>
2014-12-03 19:47:40 -0500

    Added more authorization functions.
        - Required users to be signed in for settings editing.
        - Added friendly forwarding.
        $ git add .
        $ git commit -F commit-message.txt
        $ git push origin master

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -5,16 +5,26 @@ module SessionsHelper
         elsif cookies.signed[ :user_id ]
             user = User.find_by( :id => cookies.signed[ :user_id ] )
             if user && user.authenticated?( cookies[ :remember_token ] )
-                log_in user
+                signin user
                 @current_user = user
             end
         end
+    end
+    
+    def current_user?( user )
+        user == current_user
     end
     
     def forget( user )
         user.forget
         cookies.delete( :user_id )
         cookies.delete( :remember_token )
+    end
+    
+    # Friendly forwards the user, then deletes the now unnecessary storage variable.
+    def redirect_back_or( default )
+        redirect_to( session[ :forwarding_url ] || default )
+        session.delete( :forwarding_url )
     end
     
     def remember( user )
@@ -39,5 +49,10 @@ module SessionsHelper
         forget( current_user )
         session.delete( :user_id )
         @current_user = nil
+    end
+    
+    # Stores the URL the user tries to access for friendly forwarding purposes.
+    def store_location
+        session[ :forwarding_url ] = request.url if request.get?
     end
 end
