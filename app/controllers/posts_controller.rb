abeb2b42a3e6c1b7713c1990c451cfdfc7ca0723
Farrah Brink <febrink@gmail.com>
2015-05-18 23:44:00 -0400

    Add a user activity feed.
        $ rails generate model Post
        $ rails generate controller Posts
        - Set up the Post object, which is created and displayed on the user
          show page.
        - Wrote validations, filters, and form errors for Post objects.

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,37 @@
+class PostsController < ApplicationController
+
+    include AuthorizationFilters
+    
+    before_action :owner_user, :only => [ :destroy ]
+    before_action :signed_in_user, :only => [ :create ]
+
+    def create
+        @post = Post.new( post_params )
+        respond_to do | format |
+            if @post.save
+                @saved = true
+                format.html { redirect_to user_path( params[ :user_id ] ) }
+            else
+                @saved = false
+            end
+            format.js { render layout: false, content_type: 'text/javascript' }
+        end
+    end
+    
+    def destroy
+        @post = Post.find( params[ :id ] )
+        @post.destroy
+        redirect_to :back
+    end
+
+    private
+    
+        def owner_user
+            owner = User.find( params[ :user_id ] )
+            redirect_to root_path unless current_user?( owner )
+        end
+    
+        def post_params
+            params.require( :post ).permit( :content ).merge( params.permit( :user_id ) )
+        end
+end
