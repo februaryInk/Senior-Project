d7c7a55b5f451c5a2a3e6ef9c1dd7b34c4d4979f
Farrah Brink <febrink@gmail.com>
2015-05-25 11:48:00 -0400

    Expand user model test.

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -3,6 +3,7 @@ ENV[ 'RAILS_ENV' ] ||= 'test'
 require File.expand_path( '../../config/environment', __FILE__ )
 require 'rails/test_help'
 require 'minitest/reporters'
+require 'win32console'
 Minitest::Reporters.use!
 
 class ActiveSupport::TestCase
