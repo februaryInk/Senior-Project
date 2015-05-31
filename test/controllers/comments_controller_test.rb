07c306035ee6c795a006236feb1ab56b0338ccac
Farrah Brink <febrink@gmail.com>
2015-05-30 20:18:55 -0400

    Expand comments controller tests.

--- test/controllers/comments_controller_test.rb
+++ test/controllers/comments_controller_test.rb
@@ -1,7 +1,34 @@
 require 'test_helper'
 
 class CommentsControllerTest < ActionController::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+    
+    def setup
+        @conversation_1 = conversations( :conversation_1 )
+        @test_user = users( :test_user )
+    end
+    
+    # CREATE
+    
+    test 'should create a comment' do
+        sign_in_as @test_user
+        assert_difference 'Comment.count', 1 do
+            post :create, :comment => { :content => 'alpha', :conversation_id => @conversation_1.id, :user_id => @test_user.id }, :format => 'js'
+        end
+        assert_equal 'alpha', Comment.last.content
+    end
+    
+    test 'should redirect if no user is signed in' do
+        assert_no_difference 'Comment.count' do
+            post :create, :comment => { :content => 'alpha', :conversation_id => @conversation_1.id, :user_id => @test_user.id }, :format => 'js'
+        end
+        assert_redirected_to signin_path
+    end
+    
+    test 'should assert errors with incomplete information' do
+        sign_in_as @test_user
+        assert_no_difference 'Comment.count' do
+            post :create, :comment => { :content => '', :conversation_id => @conversation_1.id, :user_id => @test_user.id }, :format => 'js'
+        end
+        assert_template 'comments/_new'
+    end
 end
