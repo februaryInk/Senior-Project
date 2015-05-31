34216a482bf069b5852fb0faa3a1120c965c2be7
Farrah Brink <febrink@gmail.com>
2015-05-31 14:23:27 -0400

    Expand core_pages and help_pages controller tests.

--- test/controllers/core_pages_controller_test.rb
+++ test/controllers/core_pages_controller_test.rb
@@ -2,15 +2,21 @@ require 'test_helper'
 
 class CorePagesControllerTest < ActionController::TestCase
 
+    # ABOUT
+
     test 'should get about' do
         get :about
         assert_response :success
+        assert_template 'core_pages/about'
         assert_select 'title', about_title
     end
+    
+    # HOME
 
     test 'should get home' do
         get :home
         assert_response :success
+        assert_template 'core_pages/home'
         assert_select 'title', home_title
     end
 end
--- test/controllers/help_pages_controller_test.rb
+++ test/controllers/help_pages_controller_test.rb
@@ -2,27 +2,39 @@ require 'test_helper'
 
 class HelpPagesControllerTest < ActionController::TestCase
 
+    # CONTACT
+
     test 'should get contact' do
         get :contact
         assert_response :success
+        assert_template 'help_pages/contact'
         assert_select 'title', contact_title
     end
+    
+    # FAQ
 
     test 'should get faq' do
         get :faq
         assert_response :success
+        assert_template 'help_pages/faq'
         assert_select 'title', faq_title
     end
+    
+    # GETTING STARTRED
 
     test 'should get getting_started' do
         get :getting_started
         assert_response :success
+        assert_template 'help_pages/getting_started'
         assert_select 'title', getting_started_title
     end
+    
+    # HELP CENTER
 
     test 'should get help_center' do
         get :help_center
         assert_response :success
+        assert_template 'help_pages/help_center'
         assert_select 'title', help_title
     end
 end
